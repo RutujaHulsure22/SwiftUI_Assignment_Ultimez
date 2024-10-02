860957e0ec29af52ab51911de309a67039ecbb81
Rutuja Hulsure <hulsurerutuja22@gmail.com>
2024-10-02 18:51:46 +0530

    Initial Commit

---
+++ SwiftUI_Assignment_Ultimez/SwiftUI_Assignment_UltimezApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Assignment_UltimezApp.swift
+//  SwiftUI_Assignment_Ultimez
+//
+//  Created by Coditas on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Assignment_UltimezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
